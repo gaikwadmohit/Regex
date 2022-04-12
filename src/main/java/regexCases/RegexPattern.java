81d95bf43a890482d285b0fe67380bd73fe427eb
unknown <mohit16gaikwad@gmail.com>
2022-04-12 11:54:54 +0530

    Mobile Number Matching Successfully

--- src/main/java/regexCases/RegexPattern.java
+++ src/main/java/regexCases/RegexPattern.java
@@ -40,5 +40,42 @@ public class RegexPattern {
 			System.out.println();
 
 		}
+		public void Email_Id() {
+
+			// scanning
+			String line = "abc.xyz@bl.co.in";
+			String pattern = "^[a-z]\\.[a-z][@][a-z]\\.[a-z]\\.[a-z]*";
+
+			// Create pattern object
+			Pattern pat = Pattern.compile(pattern);
+
+			// matching object create
+			Matcher mat = pat.matcher(line);
+
+			// Check Matching or not
+			boolean result2 = mat.matches();
+			System.out.println("Email_Id Matching result is " + result2);
+			System.out.println();
+
+		}
+		public void Mobile_Number() {
+
+			// scanning
+			String line = "+91 8859652365";
+			String pattern = "^([+]\\d{2}[ ])?\\d{10}$";
+
+			// Create pattern object
+			Pattern pat = Pattern.compile(pattern);
+
+			// matching object create
+			Matcher mat = pat.matcher(line);
+
+			// Check Matching or not
+			boolean result3 = mat.matches();
+			System.out.println("Mobile Number Matching result is " + result3);
+			System.out.println();
+
+		}
 }
+
 
